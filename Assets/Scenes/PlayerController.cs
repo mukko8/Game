62c9e006b430841c5cf60a2119c435d5ba031159
ttaguchi <ozaki6626pyahoo.co.jp>
2024-04-05 11:29:32 +0900

    playercontrollerとcameracontroller

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    //移動量初期化
+    Vector3 moveDirection = Vector3.zero;
+
+    public float gravity;
+    public float speed;
+    public float speedJump;
+    public float rotateSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //左右に回転
+            if(Input.GetAxis("Mouse X")!=0){
+                transform.Rotate(0,Input.GetAxis("Mouse X")*rotateSpeed ,0 );
+            }
+        if(controller.isGrounded){
+            //前後左右に移動
+            moveDirection.z = Input.GetAxis("Vertical") * speed;       
+            moveDirection.x = Input.GetAxis("Horizontal") * speed;       
+             
+            //ジャンプ
+            if(Input.GetButton("Jump")){
+                moveDirection.y=speedJump;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection*Time.deltaTime);
+
+        if(controller.isGrounded) moveDirection.y = 0;
+    }
+}
