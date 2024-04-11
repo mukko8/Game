09a516bb760f30b76de572cd2338beb8d77bcd3a
steel <gawct.231020@gmail.com>
2024-04-11 13:42:04 +0900

    Canonball2,3追加

--- Assets/Scripts/CanonballController.cs
+++ Assets/Scripts/CanonballController.cs
@@ -15,12 +15,8 @@ public class CanonballController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // プレイヤーとの距離を計算
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        if(distanceToPlayer < 30.0f){
-            transform.Translate(0,0,0.15f);//発射スピード
-            Destroy(gameObject,1);
-        }
+        transform.Translate(0,0,0.15f);//発射スピード
+        Destroy(gameObject,1);
     }
-    
+
 }
