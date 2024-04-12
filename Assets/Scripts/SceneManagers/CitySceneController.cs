fa6f284f174d434b00eefaf68705d18cc975b6c7
guniguni <guniguni513@gmail.com>
2024-04-12 10:53:07 +0900

    各Sceneに移動するスクリプト作成

---
+++ Assets/Scripts/SceneManagers/CitySceneController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CitySceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene",2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadSceneAsync("Mountain");
+    }
+
+}
