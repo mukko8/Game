21c380fdc57e8603a0bf59cdf6b28108c257b8cb
steel2 <taro.shimashima@gmail.com>
2024-04-14 18:05:36 +0900

    AttackController修正2

--- Assets/Scripts/Enemy/AttackController.cs
+++ Assets/Scripts/Enemy/AttackController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AttackController : MonoBehaviour
 {
     public float maxHP=100f;//最大HP
-    private float currentHP;//現在のHP
+    private float enemyHP;//現在のHP
     public float damage=10f;//与えるダメージ
     //playerのHP
     /*
@@ -14,7 +14,7 @@ public class AttackController : MonoBehaviour
     */
     public float attackInterval = 20; // 攻撃間隔
     private void OnTriggerEnter(Collider other){
-        if (other.gameObject.CompareTag("Player")&&currentHP!=0)//currentHPはplayer側の変数に変更
+        if (other.gameObject.CompareTag("Player")&&enemyHP!=0)//enemyHPはplayer側の変数に変更
         {
             //if (Time.frameCount % attackInterval == 0){
             //animator.SetTrigger("attack");
@@ -26,19 +26,19 @@ public class AttackController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentHP=maxHP;//初期HPを最大HPに設定
+        enemyHP=maxHP;//初期HPを最大HPに設定
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyHP<=0){
+            Destroy(gameObject);
+        }
     }
     public void TakeDamage(float damage){
-        // 敵のHPをプレイヤーのatk分、減少させる
-       //enemyHP -= playerstates.atk
-        currentHP-=damage;//HPを減らす　//currentHPはplayer側の変数に変更
-        if(currentHP<=0){
+        enemyHP-=damage;//HPを減らす　//enemyHPはplayer側の変数に変更
+        if(enemyHP<=0){
             Destroy(gameObject);
         }
     }
