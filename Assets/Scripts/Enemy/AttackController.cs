9fb387f2884f8463a5d09de09710ced37dd87152
steel <gawct.231020@gmail.com>
2024-04-15 14:46:32 +0900

    cityのNavMesh追加

--- Assets/Scripts/Enemy/AttackController.cs
+++ Assets/Scripts/Enemy/AttackController.cs
@@ -9,14 +9,22 @@ public class AttackController : MonoBehaviour
     public int damage=10;//与えるダメージ
     public float attackInterval = 20; // 攻撃間隔
     private GameObject player; // GameObject "player" を定義
-    private void OnTriggerEnter(Collider other){
+    private float timer;
+    private void OnTriggerStay(Collider other){
         int pc = other.GetComponent<PlayerController>().playerHp;
+        Debug.Log(pc);
         if (other.gameObject.CompareTag("Player")&&pc!=0)//enemyHPはplayer側の変数に変更
         {
-            if (Time.frameCount % attackInterval == 0){
+            Debug.Log("第１のif文");
+            timer +=Time.deltaTime*10;
+            Debug.Log(timer);
+            if (timer>=attackInterval){
+                timer=0;
+                Debug.Log("第２のif文");
                 //animator.SetTrigger("attack");
                 //Debug.Log("攻撃");
                 pc-=damage;//HPを減らす　//enemyHPはplayer側の変数に変更
+                Debug.Log("ダメージを受けたpcのHp"+pc);
                 if(pc<=0){
                     Destroy(other.gameObject);
                 }
