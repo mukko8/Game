ec29ddff8f21cdd36ce0bbbbd9a52a008714432f
steel <gawct.231020@gmail.com>
2024-04-15 09:40:09 +0900

    朝

--- Assets/Scripts/Enemy/AttackController.cs
+++ Assets/Scripts/Enemy/AttackController.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 public class AttackController : MonoBehaviour
 {
     public float maxHP=100f;//最大HP
-    private float enemyHP;//現在のHP
+    public float enemyHP;//現在のHP
     public float damage=10f;//与えるダメージ
     //playerのHP
-    /*
-    PlayerHealth playerHealth = FindObjectOfType<PlayerHealth>();
-    float currentPlayerHP = playerHealth.playerHP
-    */
+    
+    PlayerController pc = FindObjectOfType<PlayerController>().playerHp;
     public float attackInterval = 20; // 攻撃間隔
     private void OnTriggerEnter(Collider other){
         if (other.gameObject.CompareTag("Player")&&enemyHP!=0)//enemyHPはplayer側の変数に変更
         {
-            //if (Time.frameCount % attackInterval == 0){
-            //animator.SetTrigger("attack");
-            Debug.Log("攻撃");
-            TakeDamage(damage);
-            //}
+            if (Time.frameCount % attackInterval == 0){
+                //animator.SetTrigger("attack");
+                Debug.Log("攻撃");
+                TakeDamage(damage);
+            }
         }
     }
     // Start is called before the first frame update
